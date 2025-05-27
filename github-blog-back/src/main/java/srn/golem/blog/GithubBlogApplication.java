77a245c22699574e96f44c7daf2fd998dce70025
서상진 (Seo Sangjin) <67358565+srn13542@users.noreply.github.com>
2025-05-27 17:04:54 +0900

    뭔가 열심히 하는데 이상함

---
+++ github-blog-back/src/main/java/srn/golem/blog/GithubBlogApplication.java
@@ -0,0 +1,13 @@
+package srn.golem.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubBlogApplication.class, args);
+	}
+
+}
